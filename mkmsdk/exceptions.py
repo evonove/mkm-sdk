135376b38a9400fc61a517ffa56297369da979f3
Silvano Cerza <silvanocerza@gmail.com>
2015-02-11 17:16:09 +0100

    Created all necessary exceptions and relative tests

--- mkmsdk/exceptions.py
+++ mkmsdk/exceptions.py
@@ -5,6 +5,88 @@ class ConnectionError(Exception):
         self.message = message
 
     def __str__(self):
-        message = 'Request failed'
-
+        message = 'Request failed.'
+        if hasattr(self.response, 'status_code'):
+            message = '{}{}'.format(message, ' Status code: %s.' % self.response.status_code)
+        if hasattr(self.response, 'reason'):
+            message = '{}{}'.format(message, ' Response message: %s.' % self.response.reason)
+        if self.content:
+            message = '{}{}'.format(message, ' Error message: %s' % str(self.content))
         return message
+
+class Redirection(ConnectionError):
+    """
+    3xx
+    """
+    def __str__(self):
+        message = super(Redirection, self).__str__()
+        if self.response.get('Location'):
+            message = "{} => {}" .format(message, self.response.get('Location'))
+        return message
+
+class MissingParam(TypeError):
+    pass
+
+class MissingConfig(Exception):
+    pass
+
+class ClientError(ConnectionError):
+    """
+    4xx Client Error
+    """
+    pass
+
+class BadRequest(ClientError):
+    """
+    400 Bad Request
+    """
+    pass
+
+class UnauthorizedAccess(ClientError):
+    """
+    401 Unauthorized
+    """
+    pass
+
+class ForbiddenAccess(ClientError):
+    """
+    403 Forbidden
+    """
+
+class ResourceNotFound(ClientError):
+    """
+    404 Not Found
+    """
+    pass
+
+class ResourceConflict(ClientError):
+    """
+    409 Conflict
+    """
+    pass
+
+class ResourceGone(ClientError):
+    """
+    410 Gone
+    """
+    pass
+
+class ResourceInvalid(ClientError):
+    """
+    422 Invalid
+    """
+    pass
+
+class ServerError(ConnectionError):
+    """
+    5xx Server Error
+    """
+    pass
+
+class MethodNotAllowed(ClientError):
+    """
+    405 Method Not Allowed
+    """
+
+    def allowed_methods(self):
+        return self.response['Allow']
--- tests/test_exception.py
+++ tests/test_exception.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 import unittest
-from mkmsdk.exceptions import ConnectionError
+from mkmsdk.exceptions import ConnectionError, Redirection, ResourceNotFound, UnauthorizedAccess, MissingParam
 
 
 class TestExceptions(unittest.TestCase):
@@ -10,4 +10,28 @@ class TestExceptions(unittest.TestCase):
 
     def test_connection(self):
         error = ConnectionError({})
-        self.assertEqual(str(error), 'Request failed')
+        self.assertEqual(str(error), 'Request failed.')
+
+    def test_redirect(self):
+        error = Redirection({'Location': 'https://example.com'})
+        self.assertEqual(str(error), 'Request failed. => https://example.com')
+
+    def test_not_found(self):
+        response = self.Response(status_code='404', reason='Not found')
+        error = ResourceNotFound(response)
+        self.assertEqual(str(error), 'Request failed. Status code: %s. Response message: %s.'
+                         % (response.status_code, response.reason))
+
+    def test_unauthorized_access(self):
+        response = self.Response(status_code='401', reason='Unauthorized')
+        error = UnauthorizedAccess(response)
+        self.assertEqual(str(error), 'Request failed. Status code: %s. Response message: %s.'
+                         % (response.status_code, response.reason))
+
+    def test_missing_param(self):
+        error = MissingParam('Missing Payment Id')
+        self.assertEqual(str(error), 'Missing Payment Id')
+
+    def test_missing_config(self):
+        error = MissingParam('Missing client_id')
+        self.assertEqual(str(error), 'Missing client_id')
