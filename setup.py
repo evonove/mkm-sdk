511c0c6f6e6b96efc0d5fc1ba912f941446b2be1
Silvano Cerza <silvanocerza@gmail.com>
2015-02-12 17:53:29 +0100

    Added setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+    name='mkmsdk',
+    author='Evonove',
+    author_email='dev@evonove.it',
+    packages=['mkmsdk'],
+    scripts=[],
+    url='https://evonove.it',
+    license='MIT',
+    description='MagicKardMarket base sdk.',
+    long_description="""
+    MagicKardMarket sdk with use of some endpoints.
+    """,
+    install_requires=['requests'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    keywords="mkm magickardmarket magiccardmarket sdk",
+)
