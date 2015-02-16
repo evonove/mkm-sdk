fbbe4cf046d201b4194c7d5309f2045b0e92e9e6
Silvano Cerza <silvanocerza@gmail.com>
2015-02-16 15:35:58 +0100

    Modified setup

--- setup.py
+++ setup.py
@@ -1,18 +1,38 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
-from distutils.core import setup
+import sys
+import os
+
+version = '0.1.0'  # When changing this, please take a moment for doing the same in docs/conf.py
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    print("You probably want to also tag the version now:")
+    print(" git tag -a %s -m 'version %s'" % (version, version))
+    print(" git push --tags")
+    sys.exit()
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+
 setup(
     name='mkmsdk',
-    version='0.1.0',
+    version=version,
     author='Evonove',
     author_email='dev@evonove.it',
     packages=['mkmsdk', 'tests'],
     scripts=[],
     url='https://evonove.it',
     license='MIT',
-    description='MagicKardMarket base sdk.',
+    description='MagicKardMarket base sdk',
     long_description="""
-    Magic Kard Market sdk with use of some endpoints.
+    Magic Kard Market sdk with use of some endpoints
     """,
     install_requires=['requests', 'requests_oauthlib'],
     classifiers=[
