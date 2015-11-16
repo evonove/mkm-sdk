16b25b842d068fd2b42d7a9f4c8ed79669483110
Silvano Cerza <silvanocerza@gmail.com>
2015-11-16 17:46:08 +0100

    Integration tests are now skipped correctly only when necessary vars are not found

--- tests/tests_integration/__init__.py
+++ tests/tests_integration/__init__.py
@@ -1,9 +1,17 @@
 import unittest
 
+import mkmsdk
+
 
 def skip_integration():
-    return True
-
+    try:
+        mkmsdk.get_mkm_app_token()
+        mkmsdk.get_mkm_app_secret()
+        mkmsdk.get_mkm_access_token()
+        mkmsdk.get_mkm_access_token_secret()
+        return False
+    except mkmsdk.exceptions.MissingConfig:
+        return True
 
 @unittest.skipIf(skip_integration(), "Missing env vars, skipping integration test")
 class IntegrationTest(unittest.TestCase):
