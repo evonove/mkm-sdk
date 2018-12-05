383d54e074b24acf0680c1bbebfae7ca2f36c8b6
Silvano Cerza <silvanocerza@gmail.com>
2018-12-05 15:26:34 +0100

    Added support for 3.6 and 3.7 and removed 2.7 and 3.3

--- setup.py
+++ setup.py
@@ -46,10 +46,10 @@ setup(
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
     keywords="mkm magickardmarket magiccardmarket sdk",
